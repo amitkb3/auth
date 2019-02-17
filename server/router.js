e708741ff76266e0a6af1f73f9269e61f4da2a4c
Amit Bothra <amit.bothra@outlook.com>
2019-02-17 06:04:17 -0600

    added authentication controller

--- server/router.js
+++ server/router.js
@@ -1,6 +1,5 @@
+const Authentication =require('./controllers/authentication');
+
 module.exports = function(app) {
-  app.get('/', function(req,res,next) {
-    res.send(['WelCome', 'To', 'My', 'Server']);
-    
-  });
+  app.post('/signup', Authentication.signup);
 }
