456026d70bc555a2926b4e3e69e9363edbea8515
Amit Bothra <amit.bothra@outlook.com>
2019-02-17 06:49:46 -0600

    create new user account

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,13 +1,32 @@
+const User = require('../models/user');
+
 exports.signup = function(req,res,next) {
+
+   //Pull off email and password from incoming request
    const email = req.body.email;
    const password = req.body.password;
 
    // See if a user with a given email exist
-    
-   // If a user with email does exist, return error
+    User.findOne({ email: email }, function(err, existingUser) {
+      // connection with database failed
+      if (err) {return next(err); }
 
-   //  If a user with email does NOT exist, create and save record
+      // If a user with email does exist, return error
+      if(existingUser) {
+         return res.status(422).send({ error: 'Email is in use'});
+      }
 
-   // Respond to request indicating th user was created
+      //  If a user with email does NOT exist, create and save record
+      const user = new User({
+          email: email,
+          password: password
+      });
+
+      user.save(function(err) {
+        if (err) { return next(err); }
+      });
+      // Respond to request indicating th user was created
+      res.json({ sucess: true });
+    }); 
 
 }
