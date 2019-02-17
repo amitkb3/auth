e7b9fa8ef93b7c352f2320653d0a4830c2d686db
Amit Bothra <amit.bothra@outlook.com>
2019-02-17 06:15:15 -0600

    pull email & pwd from post requests

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,3 +1,13 @@
 exports.signup = function(req,res,next) {
-   res.send({success: 'true'});
+   const email = req.body.email;
+   const password = req.body.password;
+
+   // See if a user with a given email exist
+    
+   // If a user with email does exist, return error
+
+   //  If a user with email does NOT exist, create and save record
+
+   // Respond to request indicating th user was created
+
 }
