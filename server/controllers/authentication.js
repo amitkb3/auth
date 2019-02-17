75a0f36780c4b576b795d15dfc982385206f0852
Amit Bothra <amit.bothra@outlook.com>
2019-02-17 06:54:25 -0600

    added validation to require both email and password

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -6,6 +6,12 @@ exports.signup = function(req,res,next) {
    const email = req.body.email;
    const password = req.body.password;
 
+   if(!email || !password) {
+      return res.status(422).send({ 
+         error: 'You must provide email and password'
+      });
+   }
+
    // See if a user with a given email exist
     User.findOne({ email: email }, function(err, existingUser) {
       // connection with database failed
