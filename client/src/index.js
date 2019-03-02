2d3e3e919987aef5905340c701a5f31a2f7c60b8
Amit Bothra <amit.bothra@outlook.com>
2019-03-02 06:13:05 -0600

    enabled persistent login state

--- client/src/index.js
+++ client/src/index.js
@@ -13,7 +13,7 @@ import reducers from './reducers';
 
 const store = createStore(
   reducers,
-  {},
+  { auth: {authenticated: localStorage.getItem('token')} },
   applyMiddleware(reduxThunk)
 
 );
