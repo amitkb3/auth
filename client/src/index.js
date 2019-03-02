5c37e96aefb6b47d60bf3197228dd132e162cc41
Amit Bothra <amit.bothra@outlook.com>
2019-03-02 06:35:52 -0600

    added Signin Action creator

--- client/src/index.js
+++ client/src/index.js
@@ -10,6 +10,7 @@ import Welcome from './components/Welcome';
 import Signup from './components/auth/Signup';
 import Feature from './components/Feature';
 import Signout from './components/auth/Signout';
+import Signin from './components/auth/Signin';
 import reducers from './reducers';
 
 const store = createStore(
@@ -27,6 +28,7 @@ ReactDOM.render(
        <Route path="/signup" component={Signup}/>
        <Route path="/feature" component={Feature} />
        <Route path="/signout" component={Signout} />
+       <Route path="/signin" component={Signin} />
      </App>
   </BrowserRouter>
   </Provider> ,
