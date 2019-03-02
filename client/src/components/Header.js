b6f9343c19ce1578d640b1ae3e44a727ceaedefd
Amit Bothra <amit.bothra@outlook.com>
2019-03-02 06:49:24 -0600

    added header sytle css

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
-
+import './HeaderStyle.css';
 class Header extends React.Component {
   renderLinks() {
     if(this.props.authenticated) {
@@ -22,7 +22,7 @@ class Header extends React.Component {
   }
   render() {
     return(
-      <div>
+      <div className="header">
         <Link to="/">Redux Auth</Link>
         {this.renderLinks()}
       </div>
